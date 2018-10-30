0e75e98b4b1f3077a57a2ea410eb4984202bc786
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2018-10-30 16:40:37 +0100

    update to 0.8.0-SNAPSHOT

--- src/main/kotlin/FoldableFunctions.kt
+++ src/main/kotlin/FoldableFunctions.kt
@@ -1,44 +1,46 @@
 import arrow.Kind
 import arrow.core.*
+import arrow.instances.either.applicative.applicative
+import arrow.instances.either.foldable.foldable
+import arrow.instances.either.monad.monad
 import arrow.instances.monoid
+import arrow.instances.option.applicative.applicative
+import arrow.instances.option.monad.monad
 import arrow.typeclasses.Foldable
 
 interface FoldableFunctions {
+
+  fun <F> foldLeft(strKind: Kind<F, String>, FO: Foldable<F>): String =
+    with(FO) {
+      strKind.foldLeft("str: ") { base: String, value: String -> base + value }
+    }
+
+  fun <F> foldRight(strKind: Kind<F, String>, FO: Foldable<F>): String =
+    with(FO) {
+      strKind.foldRight(Eval.now("str: ")) { value: String, base: Eval<String> -> base.map { it + value } }
+        .value()
+    }
 
   fun <F> fold(strKind: Kind<F, String>, FO: Foldable<F>): String =
     with(FO) {
       strKind.fold(String.monoid())
     }
 
-  fun <F> foldRight(strKind: Kind<F, String>, FO: Foldable<F>) =
+  fun <F> combineAll(strKind: Kind<F, String>, FO: Foldable<F>): String =
     with(FO) {
-      strKind.foldRight(Eval.now("str: ")) { value: String, base: Eval<String> -> base.map { it + value } }
+      strKind.combineAll(String.monoid())
+    }
+
+  fun <F> reduceLeftToOption(strKind: Kind<F, String>, FO: Foldable<F>): Option<Int> =
+    with(FO) {
+      strKind.reduceLeftToOption({ it.length }) { base: Int, value: String -> base + value.length }
+    }
+
+  fun <F> reduceRightToOption(strKind: Kind<F, String>, FO: Foldable<F>): Option<Int> =
+    with(FO) {
+      strKind.reduceRightToOption({ it.length }) { value: String, base: Eval<Int> -> base.map { it + value.length } }
         .value()
     }
-
-  fun <F> foldLeft(strKind: Kind<F, String>, FO: Foldable<F>) =
-    with(FO) {
-      strKind.foldLeft("str: ") { base: String, value: String -> base + value }
-    }
-
-  fun <F> foldM(strKind: Kind<F, String>, FO: Foldable<F>) =
-    with(FO) {
-      strKind.foldM(
-        Either.monad<String>(),
-        "str: "
-      ) { base: String, value: String -> Right(base + value) }
-    }
-
-  fun <F> foldMap(strKind: Kind<F, String>, FO: Foldable<F>): Int =
-    with(FO) {
-      strKind.foldMap(Int.monoid()) { it.length }
-    }
-
-/*  fun <F> foldMapM(strKind: Kind<F, String>, FO: Foldable<F>) =
-    with(FO) {
-         strKind.foldMapM(Option.monad(), Option.monoid(Int.semigroup())) { Some(it.length) }
-    }
-  }*/
 
   fun <F> reduceLeftOption(strKind: Kind<F, String>, FO: Foldable<F>): Option<String> =
     with(FO) {
@@ -51,25 +53,29 @@ interface FoldableFunctions {
         .value()
     }
 
-  fun <F> reduceRightToOption(strKind: Kind<F, String>, FO: Foldable<F>): Option<Int> =
+  fun <F> foldMap(strKind: Kind<F, String>, FO: Foldable<F>): Int =
     with(FO) {
-      strKind.reduceRightToOption({ it.length }) { value: String, base: Eval<Int> -> base.map { it + value.length } }
-        .value()
+      strKind.foldMap(Int.monoid()) { it.length }
     }
 
-  fun <F> combineAll(strKind: Kind<F, String>, FO: Foldable<F>): String =
+  fun <F> traverse(strKind: Kind<F, String>, FO: Foldable<F>): Either<Int, Unit> =
     with(FO) {
-      strKind.combineAll(String.monoid())
+      strKind.traverse_(Either.applicative<Int>()) { Right(it.length) }
+    }.fix()
+
+  fun <F> sequence(strKind: Kind<F, Kind<ForOption, String>>, FO: Foldable<F>): Option<Unit> =
+    with(FO) {
+      strKind.sequence_(Option.applicative())
+    }.fix()
+
+  fun <F> find(strKind: Kind<F, String>, FO: Foldable<F>): Option<String> =
+    with(FO) {
+      strKind.find { it.isNotBlank() }
     }
 
   fun <F> exists(strKind: Kind<F, String>, FO: Foldable<F>): Boolean =
     with(FO) {
       strKind.exists { it.isNotBlank() }
-    }
-
-  fun <F> find(strKind: Kind<F, String>, FO: Foldable<F>): Option<String> =
-    with(FO) {
-      strKind.find { it.isNotBlank() }
     }
 
   fun <F> forAll(strKind: Kind<F, String>, FO: Foldable<F>): Boolean =
@@ -87,20 +93,23 @@ interface FoldableFunctions {
       strKind.nonEmpty()
     }
 
-  fun <F> traverse(strKind: Kind<F, String>, FO: Foldable<F>): Kind<EitherPartialOf<Int>, Unit> =
-    with(FO) {
-      strKind.traverse_(Either.applicative<Int>()) { Right(it.length) }
-    }
-
   fun <F> size(strKind: Kind<F, String>, FO: Foldable<F>): Long =
     with(FO) {
       strKind.size(Long.monoid())
     }
 
-  fun <F> sequence(strKind: Kind<F, Kind<ForOption, String>>, FO: Foldable<F>): Kind<ForOption, Unit> =
+  fun <F> foldMapM(strKind: Kind<F, String>, FO: Foldable<F>): Option<Int> =
     with(FO) {
-      strKind.sequence_(Option.applicative())
-    }
+      strKind.foldMapM(Option.monad(), Int.monoid()) { Some(it.length) }
+    }.fix()
+
+  fun <F> foldM(strKind: Kind<F, String>, FO: Foldable<F>): Either<String, String> =
+    with(FO) {
+      strKind.foldM(
+        Either.monad<String>(),
+        "str: "
+      ) { base: String, value: String -> Right(base + value) }
+    }.fix()
 
   fun foldableGet(strKind: EitherOf<String, String>): Option<String> =
     with(Either.foldable<String>()) {
--- src/main/kotlin/FoldableSample.kt
+++ src/main/kotlin/FoldableSample.kt
@@ -1,22 +1,15 @@
 import arrow.core.*
 import arrow.data.ListK
-import arrow.data.foldable
 import arrow.data.k
+import arrow.instances.listk.foldable.foldable
+import arrow.instances.option.foldable.foldable
 
 object FoldableSample : FoldableFunctions {
   @JvmStatic
   fun main(args: Array<String>) {
 
-    val maybeStr = Some("abc")
-    val maybeStrOpt = Some("abc".some())
-    val strList = listOf("a", "b", "c").k()
-    val strNoneList = listOf("a".some(), None, "c".some()).k()
-    val strOptList = listOf("a".some(), "b".some(), "c".some()).k()
-    val rightStr = Either.right("abc") as Either<String, String>
-
-    println("fold maybeStr " + fold(maybeStr, Option.foldable()))
-    println("fold None " + fold(None, Option.foldable()))
-    println("fold strList " + fold(strList, ListK.foldable()))
+    val maybeStr: Option<String> = Some("abc")
+    val strList: ListK<String> = listOf("a", "b", "c").k()
 
     println("foldLeft maybeStr " + foldLeft(maybeStr, Option.foldable()))
     println("foldLeft None " + foldLeft(None, Option.foldable()))
@@ -26,13 +19,21 @@ object FoldableSample : FoldableFunctions {
     println("foldRight None " + foldRight(None, Option.foldable()))
     println("foldRight strList " + foldRight(strList, ListK.foldable()))
 
-    println("foldM maybeStr " + foldM(maybeStr, Option.foldable()))
-    println("foldM None " + foldM(None, Option.foldable()))
-    println("foldM strList " + foldM(strList, ListK.foldable()))
+    println("fold maybeStr " + fold(maybeStr, Option.foldable()))
+    println("fold None " + fold(None, Option.foldable()))
+    println("fold strList " + fold(strList, ListK.foldable()))
 
-    println("foldMap maybeStr " + foldMap(maybeStr, Option.foldable()))
-    println("foldMap None " + foldMap(None, Option.foldable()))
-    println("foldMap strList " + foldMap(strList, ListK.foldable()))
+    println("combineAll maybeStr " + combineAll(maybeStr, Option.foldable()))
+    println("combineAll None " + combineAll(None, Option.foldable()))
+    println("combineAll strList " + combineAll(strList, ListK.foldable()))
+
+    println("reduceLeftToOption maybeStr " + reduceLeftToOption(maybeStr, Option.foldable()))
+    println("reduceLeftToOption None " + reduceLeftToOption(None, Option.foldable()))
+    println("reduceLeftToOption strList " + reduceLeftToOption(strList, ListK.foldable()))
+
+    println("reduceRightToOption maybeStr " + reduceRightToOption(maybeStr, Option.foldable()))
+    println("reduceRightToOption None " + reduceRightToOption(None, Option.foldable()))
+    println("reduceRightToOption strList " + reduceRightToOption(strList, ListK.foldable()))
 
     println("reduceLeftOption maybeStr " + reduceLeftOption(maybeStr, Option.foldable()))
     println("reduceLeftOption None " + reduceLeftOption(None, Option.foldable()))
@@ -42,21 +43,30 @@ object FoldableSample : FoldableFunctions {
     println("reduceRightOption None " + reduceRightOption(None, Option.foldable()))
     println("reduceRightOption strList " + reduceRightOption(strList, ListK.foldable()))
 
-    println("reduceRightToOption maybeStr " + reduceRightToOption(maybeStr, Option.foldable()))
-    println("reduceRightToOption None " + reduceRightToOption(None, Option.foldable()))
-    println("reduceRightToOption strList " + reduceRightToOption(strList, ListK.foldable()))
+    println("foldMap maybeStr " + foldMap(maybeStr, Option.foldable()))
+    println("foldMap None " + foldMap(None, Option.foldable()))
+    println("foldMap strList " + foldMap(strList, ListK.foldable()))
 
-    println("combineAll maybeStr " + combineAll(maybeStr, Option.foldable()))
-    println("combineAll None " + combineAll(None, Option.foldable()))
-    println("combineAll strList " + combineAll(strList, ListK.foldable()))
+    println("traverse maybeStr " + traverse(maybeStr, Option.foldable()))
+    println("traverse None " + traverse(None, Option.foldable()))
+    println("traverse strList " + traverse(strList, ListK.foldable()))
 
-    println("exists maybeStr " + exists(maybeStr, Option.foldable()))
-    println("exists None " + exists(None, Option.foldable()))
-    println("exists strList " + exists(strList, ListK.foldable()))
+    val maybeStrOpt = Some("abc".some())
+    val strNoneList = listOf("a".some(), None, "c".some()).k()
+    val strOptList = listOf("a".some(), "b".some(), "c".some()).k()
+
+    println("sequence maybeStrOpt " + sequence(maybeStrOpt, Option.foldable()))
+    println("sequence None " + sequence(None, Option.foldable()))
+    println("sequence strNoneList " + sequence(strNoneList, ListK.foldable()))
+    println("sequence strOptList " + sequence(strOptList, ListK.foldable()))
 
     println("find maybeStr " + find(maybeStr, Option.foldable()))
     println("find None " + find(None, Option.foldable()))
     println("find strList " + find(strList, ListK.foldable()))
+
+    println("exists maybeStr " + exists(maybeStr, Option.foldable()))
+    println("exists None " + exists(None, Option.foldable()))
+    println("exists strList " + exists(strList, ListK.foldable()))
 
     println("forAll maybeStr " + forAll(maybeStr, Option.foldable()))
     println("forAll None " + forAll(None, Option.foldable()))
@@ -70,20 +80,22 @@ object FoldableSample : FoldableFunctions {
     println("nonEmpty None " + nonEmpty(None, Option.foldable()))
     println("nonEmpty strList " + nonEmpty(strList, ListK.foldable()))
 
-    println("traverse maybeStr " + traverse(maybeStr, Option.foldable()))
-    println("traverse None " + traverse(None, Option.foldable()))
-    println("traverse strList " + traverse(strList, ListK.foldable()))
-
     println("size maybeStr " + size(maybeStr, Option.foldable()))
     println("size None " + size(None, Option.foldable()))
     println("size strList " + size(strList, ListK.foldable()))
 
-    println("sequence maybeStrOpt " + sequence(maybeStrOpt, Option.foldable()))
-    println("sequence None " + sequence(None, Option.foldable()))
-    println("sequence strNoneList " + sequence(strNoneList, ListK.foldable()))
-    println("sequence strOptList " + sequence(strOptList, ListK.foldable()))
+    println("foldMapM maybeStr " + foldMapM(maybeStr, Option.foldable()))
+    println("foldMapM None " + foldMapM(None, Option.foldable()))
+    println("foldMapM strList " + foldMapM(strList, ListK.foldable()))
 
-    println("foldableGet " + foldableGet(rightStr))
+    println("foldM maybeStr " + foldM(maybeStr, Option.foldable()))
+    println("foldM None " + foldM(None, Option.foldable()))
+    println("foldM strList " + foldM(strList, ListK.foldable()))
+
+    val rightStr = Either.right("abc") as Either<String, String>
+
+    println("foldableGet rightStr" + foldableGet(rightStr))
+
   }
 
 }
